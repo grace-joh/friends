1ca85b9356c24cc7ab29df64f644be917431ef20
Grace Joh <graceehjoh@gmail.com>
2023-01-04 14:47:45 -0600

    Add friends

--- friends.rb
+++ friends.rb
@@ -1,7 +1,12 @@
-best_friend = "Thomas the Train"
+friends = ["Thomas the Train","Thor","Sean","Grace"]
+best_friend = friends[rand(3)]
+puts best_friend
 
-if best_friend == "Thomas the Train"
+if best_friend == "Thomas the Train" 
     puts "Hello friend!"
+elsif best_friend == "Thor"
+    puts "Hello Thunder God"
 else
     puts "Hello acquaintance"
 end
+
